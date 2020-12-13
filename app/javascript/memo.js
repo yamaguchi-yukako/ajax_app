fa7f196d8ca67a9093796264b627f02885c2a108
yamaguchi-yukako <y_mar@hotmail.co.jp>
2020-12-13 20:13:30 +0900

    メモ投稿機能のフロントエンド実装

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -3,11 +3,39 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
 
+function memo() {
+ const submit = document.getElementById("submit");
+ submit.addEventListener("click", (e) => {
+  const formData = new FormData(document.getElementById("form"));
+  const XHR = new XMLHttpRequest();
+  XHR.open("POST", "/posts", true);
+  XHR.responseType = "json";
+  XHR.send(formData);
+  XHR.onload = () => {
+    if (XHR.status != 200) {
+      alert(`Error ${XHR.status}: ${XHR.statusText}`);
+      return null;
+    }
+    const item = XHR.response.post;
+    const list = document.getElementById("list");
+    const formText = document.getElementById("content");
+    const HTML = `
+      <div class="post" data-id=${item.id}>
+        <div class="post-date">
+          投稿日時：${item.created_at}
+        </div>
+        <div class="post-content">
+        ${item.content}
+        </div>
+      </div>`;
+    list.insertAdjacentHTML("afterend", HTML);
+    formText.value = "";
+  };
+  e.preventDefault();
+ });
+}
+window.addEventListener("load", memo);
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
